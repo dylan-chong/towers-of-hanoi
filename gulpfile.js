fb399cc0a7147531b28ac1b01f0b80d88a80d04e
Dylan Chong <dylan.chong@icloud.com>
2016-07-02 12:58:35 +1200

    Implemented all build tasks in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,39 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const liveReload = require('livereload');
-const mkdirp = require('mkdirp');
 
 const DIR = {
   BUILD: 'build/',
-  SRC: 'app/src/'
+  APP: 'app/src/'
 };
 const FILES = {
-  HTML: DIR.SRC + 'index.html',
-  CSS: DIR.SRC + '**/*.css',
-  JS: DIR.SRC + '**/*.js'
+  HTML: DIR.APP + 'index.html',
+  CSS: DIR.APP + '**/*.scss',
+  JS: DIR.APP + '**/*.js'
 };
 
-gulp.task('default', ['build'], function () {
+gulp.task('default', ['build'], function () {});
 
+gulp.task('build', ['build-html', 'build-css',
+  'build-js'], function () {
+  
 });
 
-gulp.task('build', ['make-build-dir', 'build-html',
-  'build-css', 'build-js'], function () {
-});
-
-gulp.task('make-build-dir', function (done) {
-  mkdirp(DIR.BUILD, function (error) {
-    if (!error) console.log('Made build directory');
-    else console.error('Error making build directory at "' 
-      + DIR.BUILD + '"', error);
-    
-    done();
-  });
-});
-
-gulp.task('build-html', ['make-build-dir'], function () {
+gulp.task('build-html', function () {
   return gulp.src(FILES.HTML)
-    .pipe()
+    .pipe(gulp.dest(DIR.BUILD));
 });
 
-gulp.task('build-css', ['make-build-dir'], function () {
-
+gulp.task('build-css', function () {
+  return gulp.src(FILES.CSS)
+    .pipe(gulp.dest(DIR.BUILD));
 });
 
-gulp.task('build-js', ['make-build-dir'], function () {
-
+gulp.task('build-js', function () {
+  return gulp.src(FILES.JS)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest(DIR.BUILD));
 });
-
 
 var server = liveReload.createServer();
 server.watch(DIR.BUILD);
