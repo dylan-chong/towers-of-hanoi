27a2b47e7ca1848ab573acb513456ec53c234b3f
Dylan Chong <dylan.chong@icloud.com>
2016-12-30 12:04:53 +1300

    Got cucumber tests to run at the end of `gradle test`

---
+++ src/test/java/stepdefinitions/DiskStackListStepDefs.java
@@ -0,0 +1,14 @@
+package stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
+/**
+ * Created by Dylan on 29/12/16.
+ */
+public class DiskStackListStepDefs {
+    @Given("^a starting-game stack$")
+    public void a_starting_game_stack() throws Throwable {
+        throw new PendingException();
+    }
+}
