bb749abfbe6fc247a971bae9aa64f2d658acb30f
Dylan Chong <dylan.chong@icloud.com>
2017-01-01 13:13:29 +1300

    Added JUnit test example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Towers of Hanoi
 Mockup of the puzzle "Towers of Hanoi"
+
+Also serves as a Java-IntelliJ-Gradle project template with Cucumber and JUnit
+tests
---
+++ src/test/java/junit/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package junit;
+
+import org.junit.Test;
+
+/**
+ * Created by Dylan on 1/01/17.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void exampleTest() {
+
+    }
+}
