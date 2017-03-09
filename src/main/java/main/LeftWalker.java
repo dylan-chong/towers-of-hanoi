598890b3924591e717ba7c9c81156b2bad88d7a3
Dylan Chong <dylan.chong@icloud.com>
2017-03-09 14:46:52 +1300

    Tidied submit task

--- src/main/java/main/LeftWalker.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-/**
- * Created by Dylan on 6/03/17.
- */
-public class LeftWalker {
-}
---
+++ src/main/java/swen221/assignment1/LeftWalker.java
@@ -0,0 +1,10 @@
+package swen221.assignment1;
+
+/**
+ * Created by Dylan on 6/03/17.
+ */
+public class LeftWalker {
+    public static void main(String[] args) {
+        System.out.println("DONE");
+    }
+}
