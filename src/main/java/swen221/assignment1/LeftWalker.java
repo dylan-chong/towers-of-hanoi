e0384adffdf31452f2b5627ab38e4c85f72a4454
Dylan Chong <dylan.chong@icloud.com>
2017-03-10 20:36:09 +1300

    Temp commit for testing

--- src/main/java/swen221/assignment1/LeftWalker.java
+++ src/main/java/swen221/assignment1/LeftWalker.java
@@ -1,10 +1,20 @@
 package swen221.assignment1;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Dylan on 6/03/17.
  */
 public class LeftWalker {
     public static void main(String[] args) {
         System.out.println("DONE");
+
+        new JFrame() {{
+            getContentPane().add(new TextArea("It works!"));
+            pack();
+            setVisible(true);
+            toFront();
+        }};
     }
 }
