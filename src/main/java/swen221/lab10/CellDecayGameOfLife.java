92195f8640eb7d2e7af1751f4d4e5a894e671ccf
Dylan Chong <dylan.chong@icloud.com>
2017-06-06 09:23:15 +1200

    Task 2

--- src/main/java/swen221/lab10/CellDecayGameOfLife.java
+++ src/main/java/swen221/lab10/CellDecayGameOfLife.java
@@ -1,25 +1,44 @@
 package swen221.lab10;
 
-import static swen221.lab10.GameOfLife.*;
-
-import swen221.lab10.model.*;
+import swen221.lab10.model.Board;
+import swen221.lab10.model.Rule;
+import swen221.lab10.model.Simulation;
 import swen221.lab10.util.Pair;
 import swen221.lab10.util.Point;
 import swen221.lab10.view.BoardFrame;
+
+import static swen221.lab10.GameOfLife.DEAD;
+import static swen221.lab10.GameOfLife.neighbours;
 
 public class CellDecayGameOfLife {
 	/**
 	 * The standard rule set for Conway's "Game of Life".
 	 */
 	public static final Rule[] CellDecayRules = {
-			// TODO: The underproduction rule
+			(Pair<Point, Board> p) -> { // under pop
+				int cellState = p.second().getCellState(p.first().getX(), p.first().getY());
 
-			// TODO: The reproduction rule
-
-			// TODO: The overpopulation rule
-	
+				if (cellState == DEAD || neighbours(p) >= 2) return null;
+				return cellState + 1;
+			},
+			(Pair<Point, Board> p) -> { // next gen
+				int cellState = p.second().getCellState(p.first().getX(), p.first().getY());
+				if (neighbours(p) != 2) return null;
+				return cellState;
+			},
+			(Pair<Point, Board> p) -> { // over pop
+				int cellState = p.second().getCellState(p.first().getX(), p.first().getY());
+				if (cellState == DEAD || neighbours(p) <= 3) return null;
+				return cellState + 1;
+			},
+			(Pair<Point, Board> p) -> { // happiness
+				int cellState = p.second().getCellState(p.first().getX(), p.first().getY());
+				if (neighbours(p) != 3) return null;
+				if (cellState == 0) return 0;
+				return cellState - 1;
+			}
 	};
-	
+
 	/**
 	 * The entry point for the GameOfLife application.
 	 * 
