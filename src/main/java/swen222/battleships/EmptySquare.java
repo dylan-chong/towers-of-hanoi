c4b0b77281eee51f2e891ff6a89bfa0475deb4d1
Dylan Chong <dylan.chong@icloud.com>
2017-07-28 14:25:49 +1200

    Add lab1 files

---
+++ src/main/java/swen222/battleships/EmptySquare.java
@@ -0,0 +1,12 @@
+package swen222.battleships;
+
+import javax.swing.*;
+
+/**
+ * This class represents 
+ * @author djp
+ *
+ */
+public class EmptySquare extends GridSquare {	
+	public EmptySquare() {}
+}
---
+++ src/main/java/swen222/battleships/HitSquare.java
@@ -0,0 +1,16 @@
+package swen222.battleships;
+
+import javax.swing.ImageIcon;
+
+/**
+ * A Hit square represents a square that was bomed, and did contain a
+ * battleship.
+ * 
+ * @author djp
+ */
+public class HitSquare extends GridSquare {			
+	/**
+	 * Construct a hit square	 
+	 */
+	public HitSquare() {}
+}
