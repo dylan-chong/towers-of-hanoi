438df484b0e619af64411bd38f3301984c544966
Dylan Chong <dylan.chong@icloud.com>
2017-08-30 20:40:34 +1200

    Remove crap code

--- src/main/java/main/gui/game/GameGUIResetter.java
+++ src/main/java/main/gui/game/GameGUIResetter.java
@@ -1,16 +1,11 @@
 package main.gui.game;
 
-import main.gamemodel.Direction;
 import main.gamemodel.GameModel;
-import main.gui.cardview.GUICardFrame;
-import main.gui.cardview.GUICardName;
+import main.gui.cardframe.GUICardFrame;
+import main.gui.cardframe.GUICardName;
 import main.gui.game.celldrawers.CellDrawer;
 
 import javax.swing.*;
-import java.util.Date;
-import java.util.TimerTask;
-import java.util.Timer;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 import static main.gui.game.Events.*;
@@ -64,30 +59,5 @@ public class GameGUIResetter {
 		);
 
 		guiCardFrame.addView(gameGUIView);
-
-		GameAction[] gs = {
-				() -> gameGUIModel.getGameModel().create('a', 0),
-				() -> gameGUIModel.getGameModel().move('a', Direction.SOUTH),
-				() -> gameGUIModel.getGameModel().passTurnState(),
-				() -> gameGUIModel.getGameModel().create('c', 0),
-				() -> gameGUIModel.getGameModel().move('c', Direction.NORTH),
-				() -> gameGUIModel.getGameModel().passTurnState(),
-				() -> gameGUIModel.getGameModel().create('b', 0),
-				() -> gameGUIModel.getGameModel().react(gameGUIModel.getGameModel().getReactions().stream().findAny().orElseGet(null))
-		};
-		AtomicInteger i = new AtomicInteger(0);
-		Timer timer = new Timer();
-		timer.scheduleAtFixedRate(new TimerTask() {
-			@Override
-			public void run() {
-				if (i.get() >= gs.length) return;
-
-				SwingUtilities.invokeLater(() -> gameGUIModel.performGameAction(() -> {
-					gs[i.getAndIncrement()].perform();
-					gameGUIModel.update(null, null);
-				}));
-			}
-		}, new Date(System.currentTimeMillis() + 2000), 300);
-
 	}
 }
