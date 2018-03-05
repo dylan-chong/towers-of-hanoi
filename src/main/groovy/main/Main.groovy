690cda04c3c713ad3f29a5f79db1e3b57885b6f5
Dylan Chong <dylan.chong@icloud.com>
2018-03-05 13:23:43 +1300

    Can run groovy in main code

---
+++ src/main/groovy/main/Main.groovy
@@ -0,0 +1,7 @@
+package main
+
+class MainGroovy {
+	static void main(String[] args) {
+		throw new RuntimeException('Yay groovy runs!');
+	}
+}
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 	public static void main(String[] args) {
 		startApp();
