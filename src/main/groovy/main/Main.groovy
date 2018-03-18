487d694d82bb7aa76589bb68f3d34b129020e293
Dylan Chong <dylan.chong@icloud.com>
2018-03-19 12:19:12 +1300

    Use kotlin

--- src/main/groovy/main/Main.groovy
+++
@@ -1,7 +0,0 @@
-package main
-
-class Main {
-    static void main(String[] args) {
-        throw new RuntimeException('Yay groovy runs!')
-    }
-}
--- src/test/groovy/ExampleGroovySpec.groovy
+++
@@ -1,8 +0,0 @@
-import spock.lang.Specification
-
-class HelloSpockSpec extends Specification {
-    def "length of Spock's and his friends' names"() {
-        expect:
-        false
-    }
-}
