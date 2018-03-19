62a92824f8f8d4a8a27471d5cb5ef0261fe3c478
Dylan Chong <dylan.chong@icloud.com>
2018-03-19 16:59:56 +1300

    Implement MParallelSorter1

--- src/main/kotlin/ass1/MParallelSorter1.kt
+++ src/main/kotlin/ass1/MParallelSorter1.kt
@@ -1,9 +1,29 @@
 package ass1
 
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+
 class MParallelSorter1 : Sorter {
 
-    override fun <T : Comparable<T>> sort(list: List<T>): List<T>? {
-        return null
+    companion object {
+        const val THRESHOLD = 20
+
+        val pool = Executors.newCachedThreadPool()!!
+    }
+
+    override fun <T : Comparable<T>> sort(list: List<T>): List<T> {
+        if (list.size < THRESHOLD) {
+            return ISequentialSorter().sort(list)
+        }
+
+        val halfWay = list.size / 2
+        val firstHalf = list.subList(0, halfWay)
+        val secondHalf = list.subList(halfWay, list.size)
+
+        val firstFuture = pool.submit(Callable { sort(firstHalf) })
+        val secondSorted = sort(secondHalf)
+
+        return Merger.merge(firstFuture.get(), secondSorted)
     }
 
 }
