da3fccfa634abfc3776abc5b92908d1d62058c23
Dylan Chong <dylan.chong@icloud.com>
2018-03-19 17:29:50 +1300

    Use forkjoin pool for MParallelSorter2

--- src/main/kotlin/ass1/MParallelSorter2.kt
+++ src/main/kotlin/ass1/MParallelSorter2.kt
@@ -1,29 +1,38 @@
 package ass1
 
-import java.util.concurrent.Callable
-import java.util.concurrent.Executors
+import java.util.concurrent.ForkJoinPool
+import java.util.concurrent.RecursiveTask
 
 class MParallelSorter2 : Sorter {
-
 
     companion object {
         const val THRESHOLD = 20
 
-        val pool = Executors.newCachedThreadPool()!!
+        val pool = ForkJoinPool()
     }
 
     override fun <T : Comparable<T>> sort(list: List<T>): List<T> {
-        if (list.size < THRESHOLD) {
-            return ISequentialSorter().sort(list)
+        return pool.submit(SortTask(list)).invoke()
+    }
+
+    class SortTask<T : Comparable<T>>(private val list: List<T>)
+        : RecursiveTask<List<T>>() {
+
+        override fun compute(): List<T> {
+            if (list.size < THRESHOLD) {
+                return ISequentialSorter().sort(list)
+            }
+
+            val halfWay = list.size / 2
+            val firstHalf = list.subList(0, halfWay)
+            val secondHalf = list.subList(halfWay, list.size)
+
+            val firstFuture = SortTask(firstHalf)
+            firstFuture.fork()
+            val secondSorted = SortTask(secondHalf).compute()
+
+            return Merger.merge(firstFuture.join(), secondSorted)
         }
 
-        val halfWay = list.size / 2
-        val firstHalf = list.subList(0, halfWay)
-        val secondHalf = list.subList(halfWay, list.size)
-
-        val firstFuture = pool.submit(Callable { sort(firstHalf) })
-        val secondSorted = sort(secondHalf)
-
-        return Merger.merge(firstFuture.get(), secondSorted)
     }
 }
