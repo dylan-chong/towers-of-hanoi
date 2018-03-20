17947cd8b53713ff361e98fa5aabc21823d128db
Dylan Chong <dylan.chong@icloud.com>
2018-03-21 09:26:26 +1300

    Move tests into one file

--- src/main/kotlin/ass1/TestTask2.kt
+++ src/main/kotlin/ass1/TestTask2.kt
@@ -4,6 +4,8 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class TestTask2 : Spek({
 
@@ -11,7 +13,7 @@ class TestTask2 : Spek({
    * Copied from: http://rosettacode.org/wiki/Permutations#Kotlin
    * and modified slightly
    */
-  fun <T> permute(input: List<T>, max: Int = 200): List<List<T>> {
+  fun <T> permute(input: List<T>, max: Long = 200): List<List<T>> {
     if (input.size <= 1) return listOf(input)
     val perms = mutableListOf<List<T>>()
     val toInsert = input[0]
@@ -88,4 +90,27 @@ data class Person(val id: Int, val age: Double) : Comparable<Person> {
     }
   }
 
+}
+
+class TestTask2Extras {
+
+  @Test
+  fun mSequentialSorter_sortsFor_emptyList() {
+    assertEquals(listOf<Int>(), MSequentialSorter().sort(listOf<Int>()))
+  }
+
+  @Test
+  fun mSequentialSorter_sortsFor_oneItem() {
+    assertEquals(listOf(5), MSequentialSorter().sort(listOf(5)))
+  }
+
+  @Test
+  fun mSequentialSorter_sortsFor_twoItems() {
+    assertEquals(listOf(5, 7), MSequentialSorter().sort(listOf(7, 5)))
+  }
+
+  @Test
+  fun mSequentialSorter_sortsFor_3Items() {
+    assertEquals(listOf(1, 5, 7), MSequentialSorter().sort(listOf(7, 1, 5)))
+  }
 }
