c8f3a2d0fb34544bead0c745f971da63a4b716df
Dylan Chong <dylan.chong@icloud.com>
2018-04-13 18:35:50 +1200

    Benchmark logging

--- src/test/kotlin/Benchmarks.kt
+++ src/test/kotlin/Benchmarks.kt
@@ -2,11 +2,12 @@
 import gui.Gui
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.it
+import java.io.File
+import java.nio.file.Files
 
 class Benchmarks : Spek({
 
   it("runs and print out the run times") {
-
     val times = (1..10).map {
       // Start the simulation
       Gui.main(emptyArray())
@@ -26,7 +27,22 @@ class Benchmarks : Spek({
     }
 
     println("Milliseconds per frame results: $times")
+
     // Drop first few to allow for warm up
-    println("Milliseconds per frame average: ${times.drop(3).average()}")
+    val toDrop = 2
+    val remaining = if (times.size > toDrop) times.drop(toDrop) else times
+    val average = remaining.average()
+    println("Milliseconds per frame average: $average")
+
+    // Logging
+    val resultsFile = File("results.tmp")
+    if (!resultsFile.exists()) {
+      resultsFile.createNewFile()
+    }
+    val allResults = Files
+      .readAllLines(resultsFile.toPath())
+      .toMutableList()
+    allResults.add("{currentTime: ${System.currentTimeMillis()}, average: $average, times: $times}")
+    Files.write(resultsFile.toPath(), allResults)
   }
 })
