2e9785ca17ea533bbb9e0bf6c68f8ae0179df49e
Dylan Chong <dylan.chong@icloud.com>
2018-05-28 12:37:07 +1200

    Refactor key manager

--- src/main/java/KeyManager.java
+++ src/main/java/KeyManager.java
@@ -17,15 +17,30 @@ public class KeyManager {
   public static void main(String[] args) throws IOException {
     BigInteger nextKeyToCheck = new BigInteger(args[0]);
     int keySize = Integer.parseInt(args[1]);
-    String ciperText = args[2];
-
-    String correctKey = null;
+    String cipherText = args[2];
 
     int port = 0; // auto pick
     if (args.length > 3) {
       port = Integer.parseInt(args[3]);
     }
 
+    KeyManager keyManager = new KeyManager(nextKeyToCheck, keySize, cipherText);
+    keyManager.serve(port);
+  }
+
+  private final int keySize;
+  private final String cipherText;
+
+  private volatile String correctKey = null;
+  private volatile BigInteger nextKeyToCheck;
+
+  public KeyManager(BigInteger nextKeyToCheck, int keySize, String cipherText) {
+    this.nextKeyToCheck = nextKeyToCheck;
+    this.keySize = keySize;
+    this.cipherText = cipherText;
+  }
+
+  public void serve(int port) throws IOException {
     try (ServerSocket socket = new ServerSocket(port)) {
       System.out.println("Waiting for connections on " + socket.getLocalPort());
 
@@ -43,33 +58,44 @@ public class KeyManager {
         ) {
           String message = input.readLine();
           System.out.println("Input: " + message);
-          String[] messageSplit = message.split(" ");
-
-          if (message.startsWith(Client.ASK_FOR_WORK)) {
-            int numberOfKeysToCheck = Integer.parseInt(messageSplit[3]);
-
-            String responseMessage = String.format(
-              "%s %d %s",
-              nextKeyToCheck,
-              keySize,
-              ciperText
-            );
-            output.println(responseMessage);
-            System.out.println("Output: " + responseMessage);
-            nextKeyToCheck = nextKeyToCheck.add(
-              BigInteger.valueOf(numberOfKeysToCheck)
-            );
-          } else if (message.startsWith(Client.RESPONSE)) {
-            if (messageSplit.length >= 4) {
-              correctKey = messageSplit[3];
-            }
-          } else {
-            throw new IllegalArgumentException(message);
-          }
+          handleResponse(message, output);
         }
       }
 
       System.out.println("Correct key: " + correctKey);
     }
+  }
+
+  private synchronized void handleResponse(String message, PrintWriter output) {
+    String[] messageSplit = message.split(" ");
+
+    if (message.startsWith(Client.ASK_FOR_WORK)) {
+      nextKeyToCheck = findNextKeyToCheck(messageSplit, output, nextKeyToCheck);
+    } else if (message.startsWith(Client.RESPONSE)) {
+      if (messageSplit.length >= 4) {
+        correctKey = messageSplit[3];
+      }
+    } else {
+      throw new IllegalArgumentException(message);
+    }
+  }
+
+  private synchronized BigInteger findNextKeyToCheck(
+    String[] askMessageSplit,
+    PrintWriter output,
+    BigInteger nextKeyToCheck
+  ) {
+    int numberOfKeysToCheck = Integer.parseInt(askMessageSplit[3]);
+
+    String responseMessage = String.format(
+      "%s %d %s",
+      nextKeyToCheck,
+      keySize,
+      cipherText
+    );
+    output.println(responseMessage);
+    System.out.println("Output: " + responseMessage);
+
+    return nextKeyToCheck.add(BigInteger.valueOf(numberOfKeysToCheck));
   }
 }
