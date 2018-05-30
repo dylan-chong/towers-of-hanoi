ca23b2b33c4a9c87d03df2a03ccada42ffa264be
Dylan Chong <dylan.chong@icloud.com>
2018-05-30 12:15:24 +1200

    Duplicate client and key manager classes

---
+++ src/main/java/Task5Client.java
@@ -0,0 +1,163 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.net.Socket;
+
+/**
+ * Asks for work to do from a {@link KeyManager}, performs the work, and then
+ * returns a result. This client then goes back to the start, asking for more
+ * work to do.
+ */
+public class Client {
+
+  public static final String ASK_FOR_WORK = "ASK";
+  public static final String RESPONSE = "RESPONSE";
+
+  /**
+   * @param args[0] key manager hostname
+   * @param args[1] key manager port
+   * @param args[2] number of keys to check
+   */
+  public static void main(String[] args) throws InterruptedException {
+    String hostname = args[0];
+    int port = Integer.parseInt(args[1]);
+    int numberOfKeysToCheck = Integer.parseInt(args[2]);
+
+    Client client = new Client(hostname, port, numberOfKeysToCheck);
+
+    while (true) {
+      try {
+        client.workForKeyManager();
+      } catch (IOException e) {
+        Exception e2 = new IOException(
+          "There was a problem connecting to the KeyManager. " +
+            "Maybe the key was already found? " +
+            "Or maybe there actually was a problem connecting. ",
+          e
+        );
+        e2.printStackTrace();
+
+        Thread.sleep(1000);
+      }
+    }
+  }
+
+  private final String hostname;
+  private final int port;
+  private final int numberOfKeysToCheck;
+
+  public Client(String hostname, int port, int numberOfKeysToCheck) {
+    this.hostname = hostname;
+    this.port = port;
+    this.numberOfKeysToCheck = numberOfKeysToCheck;
+  }
+
+  private String workForKeyManager() throws IOException {
+    String askForWorkResponse = findWorkToDo();
+    String matchingKey = processKeys(askForWorkResponse);
+    return sendWorkResult(matchingKey);
+  }
+
+  private String findWorkToDo() throws IOException {
+    return talkToManager((socket, in, out) -> {
+      String message = String.format(
+        "%s %s %d %s",
+        ASK_FOR_WORK,
+        socket.getLocalAddress().getHostAddress(),
+        socket.getLocalPort(),
+        numberOfKeysToCheck
+      );
+      System.out.println("Output: " + message);
+      out.println(message);
+
+      String response = in.readLine();
+      System.out.println("Input: " + response);
+
+      return response;
+    });
+  }
+
+  private String sendWorkResult(String matchingKey) throws IOException {
+    return talkToManager((socket, in, out) -> {
+      String completionMessage = String.format(
+        "%s %s %d %s",
+        RESPONSE,
+        socket.getLocalAddress().getHostAddress(),
+        socket.getLocalPort(),
+        matchingKey == null ? "" : matchingKey
+      ).trim();
+      System.out.println("Output: " + completionMessage);
+      out.println(completionMessage);
+      return matchingKey;
+    });
+  }
+
+  private <T> T talkToManager(NetworkWork<T> work) throws IOException {
+    try (
+      Socket socket = new Socket(hostname, port);
+      BufferedReader in = new BufferedReader(
+        new InputStreamReader(socket.getInputStream())
+      );
+      PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
+    ) {
+      return work.workWith(socket, in, out);
+    }
+  }
+
+  private String processKeys(String askForWorkResponse) {
+    String[] askForWorkResponseSplit = askForWorkResponse.split(" ");
+    BigInteger startingKey = new BigInteger(askForWorkResponseSplit[0]);
+    int keySize = Integer.parseInt(askForWorkResponseSplit[1]);
+    byte[] ciphertext = Blowfish.fromBase64(askForWorkResponseSplit[2]);
+
+    return processKeys(
+      startingKey,
+      keySize,
+      ciphertext,
+      numberOfKeysToCheck
+    );
+  }
+
+  private String processKeys(
+    BigInteger bi,
+    int keySize,
+    byte[] ciphertext,
+    int numberOfKeysToCheck
+  ) {
+    byte[] key = Blowfish.asByteArray(bi, keySize);
+
+    // Extract the key, turn into an array (of right size) and
+    //   convert the base64 ciphertext into an array
+
+    // Go into a loop where we try a range of keys starting at the given one
+    String plaintext = null;
+    // Search from the key that will give us our desired ciphertext
+    for (int i = 0; i < numberOfKeysToCheck; i++) {
+      // tell user which key is being checked
+      String keyStr = bi.toString();
+      if (i % 1000 == 0) {
+        System.out.print('.');
+      }
+
+      // decrypt and compare to known plaintext
+      Blowfish.setKey(key);
+      plaintext = Blowfish.decryptToString(ciphertext);
+      if (plaintext.equals("May good flourish; Kia hua ko te pai")) {
+        System.out.println();
+        System.out.println("Plaintext found!");
+        System.out.println(plaintext);
+        System.out.println("key is (hex) " + Blowfish.toHex(key) + " " + bi);
+        return keyStr;
+      }
+
+      // try the next key
+      bi = bi.add(BigInteger.ONE);
+      key = Blowfish.asByteArray(bi, keySize);
+    }
+
+    System.out.println();
+    return null;
+  }
+}
---
+++ src/main/java/Task5KeyManager.java
@@ -0,0 +1,140 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Distributed key cracker. This class delegates the work to {@link Client}s
+ * that say they are available to do work.
+ * <p>
+ * Each time a client asks for work, a unique amount of work is given in return.
+ * The client can then message this manager later with the result of the work.
+ */
+public class KeyManager {
+
+  /**
+   * @param args[0] key represented as a big integer value
+   * @param args[1] key size
+   * @param args[2] ciphertext encoded as base64 value
+   * @param args[3] optional: port number
+   * @param args[4] optional: simulate slow network (true)
+   */
+  public static void main(String[] args) throws IOException {
+    BigInteger nextKeyToCheck = new BigInteger(args[0]);
+    int keySize = Integer.parseInt(args[1]);
+    String cipherText = args[2];
+
+    int port = 0; // auto pick
+    if (args.length > 3) {
+      port = Integer.parseInt(args[3]);
+    }
+
+    boolean simulateSlowNetwork = false;
+    if (args.length > 4) {
+      simulateSlowNetwork = Boolean.parseBoolean(args[4]);
+    }
+
+    KeyManager keyManager = new KeyManager(
+      nextKeyToCheck,
+      keySize,
+      cipherText,
+      simulateSlowNetwork
+    );
+    keyManager.serve(port);
+  }
+
+  private final int keySize;
+  private final String cipherText;
+  private final boolean simulateSlowNetwork;
+
+  private String correctKey = null;
+  private BigInteger nextKeyToCheck;
+
+  public KeyManager(
+    BigInteger nextKeyToCheck,
+    int keySize,
+    String cipherText,
+    boolean simulateSlowNetwork
+  ) {
+    this.nextKeyToCheck = nextKeyToCheck;
+    this.keySize = keySize;
+    this.cipherText = cipherText;
+    this.simulateSlowNetwork = simulateSlowNetwork;
+  }
+
+  public void serve(int port) throws IOException {
+    try (ServerSocket socket = new ServerSocket(port)) {
+      System.out.println("Waiting for connections on " + socket.getLocalPort());
+
+      while (correctKey == null) {
+        Socket client = socket.accept();
+        if (simulateSlowNetwork) {
+          Thread.sleep(1000);
+        }
+
+        serveClient(client, (socket1, in, out) -> {
+          String message = in.readLine();
+          System.out.println("Input: " + message);
+          handleResponse(message, out);
+          return null;
+        });
+      }
+
+      System.out.println("Correct key: " + correctKey);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private <T> T serveClient(
+    Socket client,
+    NetworkWork<T> work
+  ) throws IOException {
+    try (
+      BufferedReader input = new BufferedReader(
+        new InputStreamReader(client.getInputStream())
+      );
+      PrintWriter output = new PrintWriter(
+        client.getOutputStream(), true
+      )
+    ) {
+      return work.workWith(client, input, output);
+    }
+  }
+
+  private void handleResponse(String message, PrintWriter output) {
+    String[] messageSplit = message.split(" ");
+
+    if (message.startsWith(Client.ASK_FOR_WORK)) {
+      nextKeyToCheck = findNextKeyToCheck(messageSplit, output, nextKeyToCheck);
+    } else if (message.startsWith(Client.RESPONSE)) {
+      if (messageSplit.length >= 4) {
+        correctKey = messageSplit[3];
+      }
+    } else {
+      throw new IllegalArgumentException(message);
+    }
+  }
+
+  private BigInteger findNextKeyToCheck(
+    String[] askMessageSplit,
+    PrintWriter output,
+    BigInteger nextKeyToCheck
+  ) {
+    int numberOfKeysToCheck = Integer.parseInt(askMessageSplit[3]);
+
+    String responseMessage = String.format(
+      "%s %d %s",
+      nextKeyToCheck,
+      keySize,
+      cipherText
+    );
+    output.println(responseMessage);
+    System.out.println("Output: " + responseMessage);
+
+    return nextKeyToCheck.add(BigInteger.valueOf(numberOfKeysToCheck));
+  }
+}
